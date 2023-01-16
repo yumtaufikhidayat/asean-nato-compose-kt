0c70de2d16a0bf3dedb4f221dc275d673895bfcb
yumtaufikhidayat <yumtaufik1997@gmail.com>
2023-01-17 06:58:14 +0700

    Change text to italic.

--- app/src/main/java/com/taufik/aseannatocompose/ui/components/CountryListItem.kt
+++ app/src/main/java/com/taufik/aseannatocompose/ui/components/CountryListItem.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -83,7 +84,7 @@ fun CountryListItem(
                 )
                 Text(
                     text = countryInternationalName,
-                    fontWeight = FontWeight.Normal,
+                    fontStyle = FontStyle.Italic,
                     modifier = modifier.padding(end = 16.dp),
                 )
             } else {
