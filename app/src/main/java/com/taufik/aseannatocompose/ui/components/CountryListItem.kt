df86c03d1f977eea4c06f017c4204f4337e8f080
yumtaufikhidayat <yumtaufik1997@gmail.com>
2023-01-16 07:28:16 +0700

    Add preview and add some components to text.

--- app/src/main/java/com/taufik/aseannatocompose/ui/components/CountryListItem.kt
+++ app/src/main/java/com/taufik/aseannatocompose/ui/components/CountryListItem.kt
@@ -2,6 +2,8 @@ package com.taufik.aseannatocompose.ui.components
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -10,15 +12,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.taufik.aseannatocompose.ui.theme.AseanNatoComposeTheme
 
 @Composable
 fun CountryListItem(
-    modifier: Modifier = Modifier,
     context: Context = LocalContext.current,
     countryName: String = "",
     countryInternationalName: String = "",
@@ -29,12 +33,26 @@ fun CountryListItem(
     countryIndependenceDay: String  = "",
     countryLanguage: String  = "",
     countryCurrency: String  = "",
-    countryLandArea: String  = ""
+    countryLandArea: String  = "",
+    isShow: Boolean = false,
+    modifier: Modifier = Modifier
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier.clickable {
-            Toast.makeText(context, countryName, Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                context,
+                    countryName +
+                    "\n$countryInternationalName" +
+                    "\n$countryDescription" +
+                    "\n$countryHeadGovernment" +
+                    "\n$countryCapital" +
+                    "\n$countryIndependenceDay" +
+                    "\n$countryLanguage" +
+                    "\n$countryCurrency" +
+                    "\n$countryLandArea",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     ) {
         AsyncImage(
@@ -45,6 +63,10 @@ fun CountryListItem(
                 .padding(8.dp)
                 .size(60.dp)
                 .clip(CircleShape)
+                .border(
+                    border = BorderStroke(2.dp, Color(0xFF6200EE)),
+                    shape = CircleShape
+                )
         )
 
         Column(
@@ -53,17 +75,66 @@ fun CountryListItem(
                 .weight(1f)
                 .padding(horizontal = 16.dp),
         ) {
-            Text(
-                text = countryName,
-                fontWeight = FontWeight.Bold,
-                modifier = modifier.padding(end = 16.dp),
-            )
-            Text(
-                text = countryInternationalName,
-                fontWeight = FontWeight.Normal,
-                modifier = modifier.padding(end = 16.dp),
-            )
+            if (isShow) {
+                Text(
+                    text = countryName,
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(end = 16.dp),
+                )
+                Text(
+                    text = countryInternationalName,
+                    fontWeight = FontWeight.Normal,
+                    modifier = modifier.padding(end = 16.dp),
+                )
+            } else {
+                Text(
+                    text = countryDescription,
+                    fontWeight = FontWeight.Normal,
+                    modifier = modifier.padding(end = 16.dp),
+                )
+                Text(
+                    text = countryHeadGovernment,
+                    fontWeight = FontWeight.Normal,
+                    modifier = modifier.padding(end = 16.dp),
+                )
+                Text(
+                    text = countryCapital,
+                    fontWeight = FontWeight.Normal,
+                    modifier = modifier.padding(end = 16.dp),
+                )
+                Text(
+                    text = countryIndependenceDay,
+                    fontWeight = FontWeight.Normal,
+                    modifier = modifier.padding(end = 16.dp),
+                )
+                Text(
+                    text = countryLanguage,
+                    fontWeight = FontWeight.Normal,
+                    modifier = modifier.padding(end = 16.dp),
+                )
+                Text(
+                    text = countryCurrency,
+                    fontWeight = FontWeight.Normal,
+                    modifier = modifier.padding(end = 16.dp),
+                )
+                Text(
+                    text = countryLandArea,
+                    fontWeight = FontWeight.Normal,
+                    modifier = modifier.padding(end = 16.dp),
+                )
+            }
         }
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun AseanNatoAppPreview() {
+    AseanNatoComposeTheme {
+        CountryListItem(
+            countryName = "Indonesia",
+            countryInternationalName = "Republik Indonesia",
+            isShow = true
+        )
+    }
+}
