84a6c2a6d1e5817b0c2a3c95e008d2958eae8e76
yumtaufikhidayat <yumtaufik1997@gmail.com>
2023-01-16 07:52:18 +0700

    Change border stroke color.

--- app/src/main/java/com/taufik/aseannatocompose/ui/components/CountryListItem.kt
+++ app/src/main/java/com/taufik/aseannatocompose/ui/components/CountryListItem.kt
@@ -7,12 +7,12 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
@@ -64,7 +64,7 @@ fun CountryListItem(
                 .size(60.dp)
                 .clip(CircleShape)
                 .border(
-                    border = BorderStroke(2.dp, Color(0xFF6200EE)),
+                    border = BorderStroke(2.dp, MaterialTheme.colors.primaryVariant),
                     shape = CircleShape
                 )
         )
