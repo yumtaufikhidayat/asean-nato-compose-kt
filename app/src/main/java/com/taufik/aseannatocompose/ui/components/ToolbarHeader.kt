653ffb6d5291cf905724d9e179bb51c2306d90f2
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2023-01-18 07:48:03 +0700

    Add profile icon.

--- app/src/main/java/com/taufik/aseannatocompose/ui/components/ToolbarHeader.kt
+++ app/src/main/java/com/taufik/aseannatocompose/ui/components/ToolbarHeader.kt
@@ -1,33 +1,41 @@
 package com.taufik.aseannatocompose.ui.components
 
-import androidx.compose.foundation.layout.Column
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import com.taufik.aseancompose.R
 import com.taufik.aseannatocompose.ui.theme.AseanNatoComposeTheme
 
 @Composable
 fun ToolbarHeader(
     modifier: Modifier = Modifier,
-    title: String = ""
+    title: String = "",
+    context: Context = LocalContext.current,
 ) {
-    Column {
-        TopAppBar(
-            title = {
-                Text(
-                    text = title,
-                    textAlign = TextAlign.Center
-                )
-            },
-            backgroundColor = MaterialTheme.colors.primaryVariant,
-            modifier = modifier.fillMaxWidth()
-        )
-    }
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                textAlign = TextAlign.Center
+            )
+        },
+        actions = {
+            IconButton(onClick = {
+                Toast.makeText(context, "Profil", Toast.LENGTH_SHORT).show()
+            }) {
+                Icon(painter = painterResource(id = R.drawable.ic_account), contentDescription = null)
+            }
+        },
+        backgroundColor = MaterialTheme.colors.primaryVariant,
+        modifier = modifier.fillMaxWidth()
+    )
 }
 
 @Preview(showBackground = true)
