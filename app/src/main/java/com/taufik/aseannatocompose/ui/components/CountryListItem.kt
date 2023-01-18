8ae0128c48d5ec6127bc5cf528c29dd8d242cb5d
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2023-01-18 07:48:31 +0700

    Add placeholder to handle loading data.

--- app/src/main/java/com/taufik/aseannatocompose/ui/components/CountryListItem.kt
+++ app/src/main/java/com/taufik/aseannatocompose/ui/components/CountryListItem.kt
@@ -15,17 +15,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.taufik.aseannatocompose.ui.theme.AseanNatoComposeTheme
+import com.taufik.aseancompose.R
 
 @Composable
 fun CountryListItem(
     modifier: Modifier = Modifier,
     context: Context = LocalContext.current,
+    countryId: Int = 0,
     countryName: String = "",
     countryInternationalName: String = "",
     countryFlagUrl: String  = "",
@@ -43,15 +46,16 @@ fun CountryListItem(
         modifier = modifier.clickable {
             Toast.makeText(
                 context,
-                    countryName +
-                    "\n$countryInternationalName" +
-                    "\n$countryDescription" +
-                    "\n$countryHeadGovernment" +
-                    "\n$countryCapital" +
-                    "\n$countryIndependenceDay" +
-                    "\n$countryLanguage" +
-                    "\n$countryCurrency" +
-                    "\n$countryLandArea",
+                "$countryId" +
+                        "\n$countryName" +
+                        "\n$countryInternationalName" +
+                        "\n$countryDescription" +
+                        "\n$countryHeadGovernment" +
+                        "\n$countryCapital" +
+                        "\n$countryIndependenceDay" +
+                        "\n$countryLanguage" +
+                        "\n$countryCurrency" +
+                        "\n$countryLandArea",
                 Toast.LENGTH_LONG
             ).show()
         }
@@ -60,6 +64,7 @@ fun CountryListItem(
             model = countryFlagUrl,
             contentDescription = null,
             contentScale = ContentScale.Crop,
+            placeholder = painterResource(id = R.drawable.ic_solid_purple),
             modifier = Modifier
                 .padding(8.dp)
                 .size(60.dp)
