8c2feb7613a7811e8cf5858ba298ed5ac59aeeab
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2023-01-23 07:43:13 +0700

    Remove unused code, label, strings, colors, and theme.

--- app/src/main/java/com/taufik/aseannatocompose/ui/theme/Theme.kt
+++ app/src/main/java/com/taufik/aseannatocompose/ui/theme/Theme.kt
@@ -2,15 +2,9 @@ package com.taufik.aseannatocompose.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
@@ -28,7 +22,10 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun AseanNatoComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun AseanNatoComposeTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     val colors = if (darkTheme) LightColorPalette else LightColorPalette
     MaterialTheme(
         colors = colors,
